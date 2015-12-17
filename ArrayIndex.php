82eca9b3ce621ce481d94426f87b136ec78962fd
C2RUN <jose.cerda66@gmail.com>
2015-12-17 16:00:04 -0300

    Primeros cambios
    Array arreglo con índice

---
+++ ArrayIndex.php
@@ -0,0 +1,16 @@
+<?php
+
+// Array con index
+
+$lenguaje[0] = "PHP";
+$lenguaje[1] = "JAVA";
+$lenguaje[2] = "JAVASCRIPT";
+$lenguaje[3] = "HTML";
+
+echo "Me gusta " . $lenguaje[0];
+echo ", " .  $lenguaje[1];
+echo ", " .  $lenguaje[2];
+echo ", " .  $lenguaje[3];
+
+
+?>
